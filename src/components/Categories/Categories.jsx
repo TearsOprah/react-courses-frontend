943e27e2b7ec13708b3e3e2d855df59dda999c87
TearsOprah <qmeshokp@mail.ru>
2023-03-02 14:29:02 +0200

    feat: вынес категории меню в компонент, возможность выбора активной категории

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,31 @@
+import './Categories.scss'
+import {useState} from "react";
+
+export default function Categories(props) {
+
+  const categories = [
+    'Дизайн',
+    'Анимация',
+    'Разработка',
+    'Иностранные языки',
+    'Маркетинг',
+  ]
+
+  const [activeIndexCategory, setActiveIndexCategory] = useState(false)
+
+  const onClickCategory = (index) => {
+    setActiveIndexCategory(index)
+  }
+
+  return (
+    <ul className={'menu-categories'}>
+      {categories.map((category, index) =>
+        <li className={`category ${activeIndexCategory === index ? 'category-active' : ''}`}
+            key={index}
+            onClick={() => onClickCategory(index)}>
+          {category}
+        </li>
+      )}
+    </ul>
+  )
+}
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import './Menu.scss'
 import logoImage from '../../images/logo.png'
+import Categories from "../Categories/Categories";
 
 export default function Menu(props) {
   return (
@@ -8,14 +9,7 @@ export default function Menu(props) {
         <img className={'company-logo'} src={logoImage}/>
         <h2 className={'company-name'}>My Company</h2>
       </div>
-      <ul className={'menu-categories'}>
-        <li className={'category category-active'}>Категория 1</li>
-        <li className={'category'}>Категория 2</li>
-        <li className={'category'}>Категория 3</li>
-        <li className={'category'}>Категория 4</li>
-        <li className={'category'}>Категория 5</li>
-        <li className={'category'}>Категория 6</li>
-      </ul>
+      <Categories />
       <h2 className={'other-links-title'}>Тут дополнительные ссылки</h2>
       <ul className={'other-links'}>
         <li className={'link'}>Еще какая-то ссылка</li>
