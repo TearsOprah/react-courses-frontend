aae84b0c80eb194cda1d35fc438e6cc6785b92a2
TearsOprah <qmeshokp@mail.ru>
2023-03-02 20:26:59 +0200

    fix: исправлен наезд items на header при прокрутке скрола

--- src/components/ItemPopup/ItemPopup.jsx
+++ src/components/ItemPopup/ItemPopup.jsx
@@ -1,10 +1,11 @@
 import './ItemPopup.scss'
 export default function ItemPopup(props) {
-  console.log('попап сработал')
   return (
     <div className={'popup popup_type_item' + (props.isOpen && ' popup_opened')}>
-      <div>
-        <h3>Это попап</h3>
+      <div className={'popup-container'}>
+        <img className={'popup-image'} src={props.item.image}/>
+        <h3 className={'popup-title'}>{props.item.title}</h3>
+        <p className={'popup-description'}>{props.item.description}</p>
         <button onClick={props.onClose}>закрыть попап</button>
       </div>
     </div>
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -55,6 +55,60 @@ const testItems = [
     description: 'Какое-то детальное описание третьего курса. Плюсы и минусы и тд.',
     image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Stepik_logotype.png'
   },
+
+  {
+    title: 'Название первого курса',
+    description: 'Какое-то детальное описание первого курса. Плюсы и минусы и тд.',
+    image: 'https://cybersport.metaratings.ru/upload/iblock/640/640eb947e68699f0b937079c5e12bcd0.jpg'
+  },
+
+  {
+    title: 'Название второго курса',
+    description: 'Какое-то детальное описание второго курса. Плюсы и минусы и тд.',
+    image: 'https://yt3.googleusercontent.com/-pnsqu0xQYwxMhUVq-HZJHf691DEhTlEl1fZvjUtUwJIKMyTqXDBVvK7d2dSjFUTYdHFpTYvAo8=s900-c-k-c0x00ffffff-no-rj'
+  },
+
+  {
+    title: 'Название третьего курса',
+    description: 'Какое-то детальное описание третьего курса. Плюсы и минусы и тд.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Stepik_logotype.png'
+  },
+
+  {
+    title: 'Название первого курса',
+    description: 'Какое-то детальное описание первого курса. Плюсы и минусы и тд.',
+    image: 'https://cybersport.metaratings.ru/upload/iblock/640/640eb947e68699f0b937079c5e12bcd0.jpg'
+  },
+
+  {
+    title: 'Название второго курса',
+    description: 'Какое-то детальное описание второго курса. Плюсы и минусы и тд.',
+    image: 'https://yt3.googleusercontent.com/-pnsqu0xQYwxMhUVq-HZJHf691DEhTlEl1fZvjUtUwJIKMyTqXDBVvK7d2dSjFUTYdHFpTYvAo8=s900-c-k-c0x00ffffff-no-rj'
+  },
+
+  {
+    title: 'Название третьего курса',
+    description: 'Какое-то детальное описание третьего курса. Плюсы и минусы и тд.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Stepik_logotype.png'
+  },
+
+  {
+    title: 'Название первого курса',
+    description: 'Какое-то детальное описание первого курса. Плюсы и минусы и тд.',
+    image: 'https://cybersport.metaratings.ru/upload/iblock/640/640eb947e68699f0b937079c5e12bcd0.jpg'
+  },
+
+  {
+    title: 'Название второго курса',
+    description: 'Какое-то детальное описание второго курса. Плюсы и минусы и тд.',
+    image: 'https://yt3.googleusercontent.com/-pnsqu0xQYwxMhUVq-HZJHf691DEhTlEl1fZvjUtUwJIKMyTqXDBVvK7d2dSjFUTYdHFpTYvAo8=s900-c-k-c0x00ffffff-no-rj'
+  },
+
+  {
+    title: 'Название третьего курса',
+    description: 'Какое-то детальное описание третьего курса. Плюсы и минусы и тд.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Stepik_logotype.png'
+  },
 ]
 
 
