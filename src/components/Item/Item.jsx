fe3026d173d863f1742e5aada9517f949632923a
TearsOprah <qmeshokp@mail.ru>
2023-03-02 20:37:35 +0200

    fix: обработчик события для кнопки, чтоб событие клика не распространялось на родителя

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,12 +6,17 @@ export default function Item(props) {
     props.onItemClick(props.item)
   }
 
+  function handleButtonClick(ev) {
+    ev.stopPropagation()
+    console.log('button')
+  }
+
   return (
     <li className={'item'} onClick={handleItemClick}>
       <img className={'item-image'} src={props.item.image} />
       <h3 className={'item-title'}>{props.item.title}</h3>
       <p className={'item-description'}>{props.item.description}</p>
-      <button className={'item-button'}>Начать</button>
+      <button className={'item-button'} onClick={handleButtonClick}>Начать</button>
     </li>
   )
 }
