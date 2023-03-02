0d599c1fa2be24ebe095d8e14ea3a2cd7b5edf94
TearsOprah <qmeshokp@mail.ru>
2023-03-02 18:36:39 +0200

    feat: вынес header из меню, поправил стили

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
-import styles from './Header.scss';
+import './Header.scss';
+import logoImage from "../../images/logo.png";
+
 export default function Header() {
   return (
-    <header>
+    <header className={'header'}>
+      <div className={'company-info'}>
+        <img className={'company-logo'} src={logoImage}/>
+        <h2 className={'company-name'}>My Company</h2>
+      </div>
     </header>
   )
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,18 +4,16 @@ import Categories from "../Categories/Categories";
 
 export default function Menu(props) {
   return (
-    <div className={'menu'}>
-      <div className={'company-info'}>
-        <img className={'company-logo'} src={logoImage}/>
-        <h2 className={'company-name'}>My Company</h2>
+    <>
+      <div className={'menu'}>
+        <Categories />
+        <h2 className={'other-links-title'}>Тут дополнительные ссылки</h2>
+        <ul className={'other-links'}>
+          <li className={'link'}>Еще какая-то ссылка</li>
+          <li className={'link'}>Еще какая-то ссылка</li>
+          <li className={'link'}>Еще какая-то ссылка</li>
+        </ul>
       </div>
-      <Categories />
-      <h2 className={'other-links-title'}>Тут дополнительные ссылки</h2>
-      <ul className={'other-links'}>
-        <li className={'link'}>Еще какая-то ссылка</li>
-        <li className={'link'}>Еще какая-то ссылка</li>
-        <li className={'link'}>Еще какая-то ссылка</li>
-      </ul>
-    </div>
+    </>
   )
 }
