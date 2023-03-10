204761d55d2f34fa9832fb160e89eb9fbba90286
TearsOprah <qmeshokp@mail.ru>
2023-03-10 09:20:30 +0200

    feat: добавил отображение price в item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -13,9 +13,17 @@ export default function Item(props) {
 
   return (
     <li className={'item'} onClick={handleItemClick}>
-      <img className={'item-image'} src={props.item.image} />
+      <img className={'item-image'} src={props.item.image} alt={`${props.item.title} logo`}/>
       <h3 className={'item-title'}>{props.item.title}</h3>
-      <p className={'item-description'}>{props.item.description}</p>
+      <p className={'item-description'}>{props.item.description.slice(0, 110)}...</p>
+      <p className={'item-price'}>
+        {props.item.price === 'free' ? 'бесплатно' : props.item.price.toLocaleString('ru-RU', {
+          style: 'currency',
+          currency: 'RUB',
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        })}
+      </p>
       <button className={'item-button'} onClick={handleButtonClick}>Начать</button>
     </li>
   )
