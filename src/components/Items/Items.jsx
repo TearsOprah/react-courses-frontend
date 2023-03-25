ac5667eef1df62b8ffc97d9211292f42092cbe39
TearsOprah <qmeshokp@mail.ru>
2023-03-25 17:10:40 +0200

    feat: пагинация

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,17 +1,55 @@
 import './Items.scss'
 import Item from "../Item/Item";
+import {useState} from "react";
 
 export default function Items(props) {
 
+  // пагинация
+
+  const [currentPage, setCurrentPage] = useState(1)
+  // кол-во элементов на странице
+  const ITEMS_PER_PAGE = 12;
+  // общее кол-во элементов
+  const TOTAL_ITEMS = props.items.length;
+  // кол-во страниц
+  const totalPages = Math.ceil(TOTAL_ITEMS / ITEMS_PER_PAGE);
+
+  function handlePageChange(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = currentPage * ITEMS_PER_PAGE;
+
+  // return (
+  //   <ul className={'items'}>
+  //     {
+  //       props.items && props.items.map((item, index) =>
+  //         <Item key={index}
+  //               item={item}
+  //               onItemClick={props.onItemClick} />
+  //       )
+  //     }
+  //   </ul>
+  // )
+
   return (
-    <ul className={'items'}>
-      {
-        props.items && props.items.map((item, index) =>
-          <Item key={index}
-                item={item}
-                onItemClick={props.onItemClick} />
-        )
-      }
-    </ul>
-  )
+    <div className="items">
+      {props.items.slice(startIndex, endIndex).map((item, index) => (
+        <Item key={index} item={item} onItemClick={props.onItemClick} />
+      ))}
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+          <button
+            key={pageNumber}
+            className={`page-button ${pageNumber === currentPage ? 'active-page-button' : ''}`}
+            onClick={() => handlePageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,8 @@
 import './Search.scss'
+import { useState} from "react";
+
 export default function Search() {
   return (
-    <input className={'search-input'} placeholder={'Поиск'}/>
+    <input className={'search-input'} placeholder={'Поиск'} type={'text'} />
   )
 }
